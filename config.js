df6154ae8d355de91d0af7992d74f3b4147545d7
Danny Cheng <feng.cheng@Kaplan.com>
2021-01-14 14:10:56 -0500

    clear up 2

--- config.js
+++ config.js
@@ -1,6 +1,4 @@
 module.exports = {
-    //gql: "https://onelasttime.prismic.io/graphql",
-    //api: "https://onelasttime.cdn.prismic.io/api/v2",
     gql: "https://newktp.prismic.io/graphql",
     api: "https://newktp.cdn.prismic.io/api/v2",
 };
